bf7f75bd62875932412f433fe0b9d2d1c1a2de25
wbhm <wbhm@mac.com>
2024-02-06 14:40:31 -0500

    updated chatbot html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { ChatbotComponent } from './chatbot/chatbot.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatbotComponent }, // Route to the chatbot component when the root path is accessed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
